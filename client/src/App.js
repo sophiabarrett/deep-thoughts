72f955777f95e771ffa55ae6651be7265257b2a7
Sophia Barrett <sophia@yoursummit.media>
2022-02-03 02:09:18 -0700

    setup react routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import {
   ApolloProvider,
   ApolloClient,
@@ -5,12 +8,15 @@ import {
   createHttpLink,
 } from "@apollo/client";
 
-import React from "react";
-
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import NoMatch from "./pages/NoMatch";
+import SingleThought from "./pages/SingleThought";
+import Profile from "./pages/Profile";
+import Signup from "./pages/Signup";
 
 // establish link to gql server
 const httpLink = createHttpLink({
@@ -26,13 +32,19 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="flex-column justify-flex-start min-100-vh">
-        <Header />
-        <div className="container">
-          <Home />
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/profile" component={Profile} />
+            <Route exact path="/thought" component={SingleThought} />
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </Router>
     </ApolloProvider>
   );
 }
