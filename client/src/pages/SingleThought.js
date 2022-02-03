bc6837f31c44da8f91e3d5bf4a2293ab406127fc
Sophia Barrett <sophia@yoursummit.media>
2022-02-03 03:03:04 -0700

    add ReactionList component

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -1,6 +1,7 @@
 import { useQuery } from '@apollo/client';
 import { QUERY_THOUGHT } from '../utils/queries';
 import { useParams } from 'react-router-dom';
+import ReactionList from '../components/ReactionList';
 
 const SingleThought = props => {
   const { id: thoughtId } = useParams();
@@ -28,6 +29,8 @@ const SingleThought = props => {
         <p>{thought.thoughtText}</p>
       </div>
     </div>
+
+    {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
   </div>
 );
 };
